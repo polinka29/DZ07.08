de6b66c8611994d369a57c3451cc6ec38b325aaf
Polina <polinka29enp@gmail.com>
2022-08-07 09:31:26 +0300

    новое решение

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -5,15 +5,16 @@
 //44 5 78 -> 78
 //22 3 9 -> 22
 
-int a = 2;
-int b = 44;
-int c = 22;
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
 
-int max = a;
-
-if (a > max) max = a;
-if (b > max) max = b;
-if (c > max) max = c;
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
 
 Console.WriteLine("max = " + max);
-
