1b5132e73b0a550bd3924428e9faa82319811ef1
Polina <polinka29enp@gmail.com>
2022-08-07 09:33:26 +0300

    задание1

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -9,7 +9,7 @@ Console.WriteLine("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
+Console.WriteLine("Введите третье число: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
 int max = num1;
 
