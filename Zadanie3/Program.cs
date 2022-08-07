3f766d222524aecce269b0c1be5dd143a4ebcacc
Polina <polinka29enp@gmail.com>
2022-08-07 12:04:30 +0300

    файл програм

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,16 @@
+﻿// 
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num%2 == 0)
+{
+   Console.WriteLine("Четное число");
+}
+else
+{
+    Console.WriteLine("Нечетное число");
+}
